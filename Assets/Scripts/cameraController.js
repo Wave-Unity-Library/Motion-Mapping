3f283d7723ab9b4304d09f100be31aa153f5b53b
jeyip <jeremy.yip7@gmail.com>
2016-07-01 19:33:55 -0700

    First commit of motion tracking scripts

---
+++ Assets/Scripts/cameraController.js
@@ -0,0 +1,69 @@
+﻿#pragma strict
+
+var player : GameObject;
+
+private var offset : Vector3;
+private var origin : Quaternion = Quaternion.identity;
+
+function Start () {
+  // checking if device has a gyroscope
+  if (SystemInfo.supportsGyroscope) {
+    Input.gyro.enabled = true;
+    // transform.rotation = Quaternion.identity;
+  }
+  Input.compass.enabled = true;
+  offset = transform.position - player.transform.position;
+}
+
+function Update () {
+  if (Input.touchCount > 0 || origin == Quaternion.identity) {
+    origin = Input.gyro.attitude;
+  }
+
+  var eulerAngles : Vector3 = Input.gyro.attitude.eulerAngles;
+    // var gyroQuaternion : Vector3 = Quaternion.Inverse(Input.gyro.attitude).eulerAngles;
+    // transform.eulerAngles = new Vector3( -gyroQuaternion[0], -gyroQuaternion[1], gyroQuaternion[2]);
+  // transform.rotation = Quaternion.Inverse(Input.gyro.attitude);
+  // transform.rotation = Quaternion.Inverse(origin) * Input.gyro.attitude;
+  // transform.forward = transform.forward * -1;
+  // print(Input.gyro.attitude);
+  // print(Input.gyro.attitude.eulerAngles);
+  // print(Input.acceleration);
+  // var roll : Quaternion = Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.z, Vector3.forward);
+  // var target : Quaternion = Quaternion.identity;
+  var pitch : Quaternion;
+  var yaw : Quaternion;
+  var roll : Quaternion;
+
+  // if (eulerAngles.x > 45 || eulerAngles.x > 315) {
+  //   roll = Quaternion.AngleAxis(0, Vector3.forward);
+    pitch = Quaternion.AngleAxis(eulerAngles.y - 90, Vector3.left);
+    yaw = Quaternion.AngleAxis(eulerAngles.x, Vector3.up);
+  // } else if (eulerAngles.x <= 45) {
+  //   roll = Quaternion.AngleAxis(0, Vector3.forward);
+  //   pitch = Quaternion.AngleAxis(eulerAngles.y, Vector3.left);
+  //   yaw = Quaternion.AngleAxis(eulerAngles.x, Vector3.up);
+  // }
+
+    var target : Quaternion = Quaternion.identity;
+    target = yaw * target;
+    target = target * pitch;
+    // transform.localRotation = yaw * pitch;
+  // transform.Rotate (pitch, Space.Self);
+  // transform.Rotate (yaw, Space.World);
+  // var roll : Quaternion = Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.z, Vector3.forward);
+  transform.localRotation = Quaternion.Slerp(transform.rotation, target, .2);
+
+  // transform.localRotation = pitch; 
+}
+
+function LateUpdate () {
+  transform.position = player.transform.position + offset;
+}
+
+function OnGUI(){
+     GUI.Label(Rect(0,0,Screen.width,Screen.height), Input.gyro.attitude.eulerAngles + '\n' 
+      + Input.gyro.attitude + '\n' 
+      + Input.acceleration + '\n'
+      + Input.compass.magneticHeading);
+ }
---
+++ Assets/Scripts/capsuleController.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+private var origin : Quaternion = Quaternion.identity;
+
+function Start () {
+  if (SystemInfo.supportsGyroscope) {
+    Input.gyro.enabled = true;
+  }
+}
+
+function ConvertRotation(q : Quaternion) {
+
+}
+
+function Update () {
+  if (Input.touchCount > 0 || origin == Quaternion.identity) {
+    origin = Input.gyro.attitude;
+  }
+  var gyroQuaternion : Quaternion = Quaternion.Inverse(Input.gyro.attitude);
+  // var reversedData : Vector3 = new Vector3 (Input.gyro.attitude.eulerAngles.x, Input.gyro.attitude.eulerAngles.y, Input.gyro.attitude.eulerAngles.z);
+  // print (reversedData);
+  // print (Quaternion.Inverse(Input.gyro.attitude).eulerAngles);
+  // print (Input.gyro.attitude.eulerAngles);
+  // var multiplier : Quaternion = Quaternion(0, 0, .7071, -.7071);
+  // print (gyroQuaternion);
+  // print (multiplier);
+  // print (multiplier * gyroQuaternion);
+  // transform.eulerAngles = new Vector3( -gyroQuaternion[0], -gyroQuaternion[1], gyroQuaternion[2]);
+  // transform.eulerAngles = reversedData;
+  // transform.rotation = Quaternion.Euler(reversedData);
+  // gyroQuaternion *= multiplier;
+  // print(gyroQuaternion);
+  // print(Input.gyro.attitude.eulerAngles.y);
+  // print(Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.y, Vector3.up));
+  
+  var yaw : Quaternion = Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.y, Vector3.left);
+  // var pitch : Quaternion = Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.x, Vector3.up);
+  // var roll : Quaternion = Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.z, Vector3.forward);
+  transform.rotation = Quaternion.Slerp(transform.rotation, yaw, .3);
+
+  // transform.rotation = gyroQuaternion;
+}
