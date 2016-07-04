67d8d833a22001a9c8e65668eebdbd1592fc70b5
Michael Laythe <mrlaythe24@aol.com>
2016-07-04 11:41:07 -0700

    trying to incorporate opus codec for more efficient audio compression

--- Assets/Scripts/AudioTransmitterHL.cs
+++ Assets/Scripts/AudioTransmitterHL.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using FragLabs.Audio.Codecs;
+using System;
+using FragLabs.Audio.Codecs.Opus;
 
 public class AudioTransmitterHL : NetworkBehaviour {
 
 	private AudioSource aud;
-	private int length;
+	private int encodedLength;
+	private int decodedLength;
 
 	void Start () {
 		aud = GetComponent<AudioSource> ();
@@ -13,52 +17,77 @@ public class AudioTransmitterHL : NetworkBehaviour {
 
 	[Command]
 	void CmdStopRecording() {
-		short[] encoded = EncodeToNSpeex(aud.clip, out length);
-		RpcPlayAudio(encoded, length);
+		byte[] encoded = EncodeToOpus(aud.clip, aud.clip.samples, out encodedLength);
+		RpcPlayAudio(encoded, 8000, aud.clip.channels);
 	}
 		
 	[ClientRpc]
-	void RpcPlayAudio(short[] encoded, int length) {
-		AudioClip a = DecodeFromNSpeex(encoded, length);
-		GetComponent<AudioSource> ().clip = a;
-		GetComponent<AudioSource> ().Play ();
+	void RpcPlayAudio(byte[] encoded, int frequency, int channels) {
+		byte[] decodedByte = DecodeFromOpus(encoded, frequency, channels);
+		float[] decodedFloat = new float[decodedByte.Length / 4];
+
+		Buffer.BlockCopy(decodedByte, 0, decodedFloat, 0, decodedByte.Length);
+
+		AudioClip a = AudioClip.Create("test", decodedFloat.Length, 1, 8000, false);
+		a.SetData(decodedFloat, 0);
+
+		GetComponent<AudioSource>().clip = a;
+		GetComponent<AudioSource>().Play();
 	}
 
-	short[] EncodeToNSpeex(AudioClip clip, out int length) {
+	byte[] EncodeToOpus(AudioClip clip, int samplesLength, out int encodedLength) {
 		float[] samplesFloat = new float[clip.samples * clip.channels];
-		short[] samplesShort = new short[clip.samples * clip.channels];
-		int sizeOfChunk = 640 * (int) (Mathf.FloorToInt(samplesFloat.Length / 640f) - 1);
-		short[] inputChunk = new short[sizeOfChunk];
-		byte[] encoded = new byte[sizeOfChunk];
-		NSpeex.SpeexEncoder m_wide_enc = new NSpeex.SpeexEncoder(NSpeex.BandMode.Wide);
+		byte[] samplesByte = new byte[samplesFloat.Length * 4];
 
 		clip.GetData(samplesFloat, 0);
-		ConvertToShort(samplesFloat, samplesShort);
 
-		for (int i = 0; i < sizeOfChunk; i++) {
-			short sample = samplesShort[i];
-			inputChunk[i] = sample;
-		}
+		Buffer.BlockCopy(samplesFloat, 0, samplesByte, 0, samplesByte.Length);
 
-		length = m_wide_enc.Encode(inputChunk, 0, inputChunk.Length, encoded, 0, encoded.Length);
+		OpusEncoder encoder = OpusEncoder.Create(8000, aud.clip.channels, Applications.Audio);
 
-		return inputChunk;
+		return encoder.Encode(samplesByte, samplesByte.Length, out encodedLength);
 	}
 
-	AudioClip DecodeFromNSpeex(short[] encoded, int length) {
-		float[] result = new float[encoded.Length];
-		short[] decoded = new short[encoded.Length];
-		NSpeex.SpeexDecoder m_wide_dec = new NSpeex.SpeexDecoder(NSpeex.BandMode.Wide);
-	
-		//yo = m_wide_dec.Decode(encoded, 0, encoded.Length, decoded, 0, false);
-
-		ConvertToFloat(encoded, result);
-
-		AudioClip a = AudioClip.Create("test", result.Length, 1, 12000, false);
-		a.SetData(result, 0);
-
-		return a;
+	byte[] DecodeFromOpus(byte[] encoded, int frequency, int channels) {
+		OpusDecoder decoder = OpusDecoder.Create(frequency, channels);
+		return decoder.Decode(encoded, encoded.Length, out decodedLength);
 	}
+
+//	short[] EncodeToNSpeex(AudioClip clip, out int length) {
+//		float[] samplesFloat = new float[clip.samples * clip.channels];
+//		short[] samplesShort = new short[clip.samples * clip.channels];
+//		int sizeOfChunk = 640 * (int) (Mathf.FloorToInt(samplesFloat.Length / 640f) - 1);
+//		short[] inputChunk = new short[sizeOfChunk];
+//		byte[] encoded = new byte[sizeOfChunk];
+//		NSpeex.SpeexEncoder m_wide_enc = new NSpeex.SpeexEncoder(NSpeex.BandMode.Wide);
+//
+//		clip.GetData(samplesFloat, 0);
+//		ConvertToShort(samplesFloat, samplesShort);
+//
+//		for (int i = 0; i < sizeOfChunk; i++) {
+//			short sample = samplesShort[i];
+//			inputChunk[i] = sample;
+//		}
+//
+//		length = m_wide_enc.Encode(inputChunk, 0, inputChunk.Length, encoded, 0, encoded.Length);
+//
+//		return inputChunk;
+//	}
+//
+//	AudioClip DecodeFromNSpeex(short[] encoded, int length) {
+//		float[] result = new float[encoded.Length];
+//		short[] decoded = new short[encoded.Length];
+//		NSpeex.SpeexDecoder m_wide_dec = new NSpeex.SpeexDecoder(NSpeex.BandMode.Wide);
+//	
+//		//yo = m_wide_dec.Decode(encoded, 0, encoded.Length, decoded, 0, false);
+//
+//		ConvertToFloat(encoded, result);
+//
+//		AudioClip a = AudioClip.Create("test", result.Length, 1, 12000, false);
+//		a.SetData(result, 0);
+//
+//		return a;
+//	}
 
 	void ConvertToShort(float[] samplesFloat, short[] samplesShort) {
 		for (int i = 0; i < samplesFloat.Length; i++) {
