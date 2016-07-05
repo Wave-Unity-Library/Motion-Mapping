4b149ef35f70593d7c6e6538192d02391dc52bff
jeyip <jeremy.yip7@gmail.com>
2016-07-05 10:40:03 -0700

    working prototype of gyroscopic camera

--- Assets/Scripts/cameraController.js
+++ Assets/Scripts/cameraController.js
@@ -3,10 +3,9 @@
 var player : GameObject;
 
 private var offset : Vector3;
-private var origin : Quaternion = Quaternion.identity;
-private var pitch : Quaternion;
-private var yaw : Quaternion;
-private var target : Quaternion = Quaternion.identity;
+private var phoneOrientation : Quaternion;
+private var orientationCorrection : Quaternion;
+private var inGameOrientation : Quaternion;
 
 function Start () {
   // Checking if device has a gyroscope to enable
@@ -18,42 +17,13 @@ function Start () {
   offset = transform.position - player.transform.position;
 }
 
-// Changes rotation of camera to reflect orientation of a phone
+// Changes rotation of camera to reflect orientation of phone
 function Update () {
-  if (Input.touchCount > 0 || origin == Quaternion.identity) {
-    origin = Input.gyro.attitude;
-  }
+  phoneOrientation = Input.gyro.attitude;
+  orientationCorrection = Quaternion.AngleAxis(-90, Vector3.left);
+  inGameOrientation = new Quaternion(phoneOrientation.x, phoneOrientation.y, -phoneOrientation.z, -phoneOrientation.w);
 
-  var eulerAngles : Vector3 = Input.gyro.attitude.eulerAngles;
-
-  pitch = Quaternion.AngleAxis(eulerAngles.y - 90, Vector3.left);
-  yaw = Quaternion.AngleAxis(eulerAngles.x, Vector3.up);
-  transform.localRotation = Quaternion.Slerp(Quaternion.identity * transform.rotation, yaw * pitch, .2);
-  // var gyroQuaternion : Vector3 = Quaternion.Inverse(Input.gyro.attitude).eulerAngles;
-  // transform.eulerAngles = new Vector3( -gyroQuaternion[0], -gyroQuaternion[1], gyroQuaternion[2]);
-  // transform.rotation = Quaternion.Inverse(Input.gyro.attitude);
-  // transform.rotation = Quaternion.Inverse(origin) * Input.gyro.attitude;
-  // transform.forward = transform.forward * -1;
-  // print(Input.gyro.attitude);
-  // print(Input.gyro.attitude.eulerAngles);
-  // print(Input.acceleration);
-  // var roll : Quaternion = Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.z, Vector3.forward);
-  // var target : Quaternion = Quaternion.identity;
-
-  // if (eulerAngles.x > 45 || eulerAngles.x > 315) {
-  //   roll = Quaternion.AngleAxis(0, Vector3.forward);
-  // } else if (eulerAngles.x <= 45) {
-  //   roll = Quaternion.AngleAxis(0, Vector3.forward);
-  //   pitch = Quaternion.AngleAxis(eulerAngles.y, Vector3.left);
-  //   yaw = Quaternion.AngleAxis(eulerAngles.x, Vector3.up);
-  // }
-    // target = yaw * target;
-    // target = target * pitch;
-    // transform.localRotation = yaw * pitch;
-  // transform.Rotate (pitch, Space.Self);
-  // transform.Rotate (yaw, Space.World);
-  // var roll : Quaternion = Quaternion.AngleAxis(Input.gyro.attitude.eulerAngles.z, Vector3.forward);
-  // transform.localRotation = pitch; 
+  transform.rotation = Quaternion.Slerp (transform.rotation, orientationCorrection * inGameOrientation, .2);
 }
 
 // Forces camera to follow player
@@ -65,6 +35,5 @@ function LateUpdate () {
 function OnGUI(){
     GUI.Label(Rect(0,0,Screen.width,Screen.height), Input.gyro.attitude.eulerAngles + '\n' 
     + Input.gyro.attitude + '\n' 
-    + Input.acceleration + '\n'
-    + Input.compass.magneticHeading);
+    + Input.acceleration + '\n');
  }
