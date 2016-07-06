c32fc56aaa95a6325b0518d8ef4cdd8820c778d7
jeyip <jeremy.yip7@gmail.com>
2016-07-06 13:57:47 -0700

    included Npseex in motion library

--- Assets/Scripts/AudioTransmitterHL.cs
+++ Assets/Scripts/AudioTransmitterHL.cs
@@ -1,141 +1,102 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
-using NAudio.Codecs;
 
 public class AudioTransmitterHL : NetworkBehaviour {
 
-    private AudioSource aud;
-    private int encodedLength;
-    private int decodedLength;
-    private int lastSamplePlayed;
+	private AudioSource aud;
+	private int length;
 
-    void Start () {
-        aud = GetComponent<AudioSource> ();
-    }
+	void Start () {
+		aud = GetComponent<AudioSource> ();
+	}
 
-    [Command]
-    void CmdStopRecording(byte[] encoded) {
-        RpcPlayAudio(encoded);
-    }
+	[Command]
+	void CmdStopRecording() {
+		short[] encoded = EncodeToNSpeex(aud.clip, out length);
+		RpcPlayAudio(encoded, length);
+	}
+		
+	[ClientRpc]
+	void RpcPlayAudio(short[] encoded, int length) {
+		AudioClip a = DecodeFromNSpeex(encoded, length);
+		GetComponent<AudioSource> ().clip = a;
+		GetComponent<AudioSource> ().Play ();
+	}
 
-    [ClientRpc]
-    void RpcPlayAudio(byte[] encoded) {
-        short[] samplesShort = new short[encoded.Length];
-        float[] samplesFloat = new float[encoded.Length];
+	byte[] EncodeToNSpeex(AudioClip clip, out int length) {
+		float[] samplesFloat = new float[clip.samples * clip.channels];
+		short[] samplesShort = new short[clip.samples * clip.channels];
+		int sizeOfChunk = 640 * (int) (Mathf.FloorToInt(samplesFloat.Length / 640f) - 1);
+		short[] inputChunk = new short[sizeOfChunk];
+		byte[] encoded = new byte[sizeOfChunk];
+		NSpeex.SpeexEncoder m_wide_enc = new NSpeex.SpeexEncoder(NSpeex.BandMode.Wide);
 
-        for (int i = 0; i < encoded.Length; i++) {
-            samplesShort [i] = MuLawDecoder.MuLawToLinearSample (encoded [i]);
-        }
+		clip.GetData(samplesFloat, 0);
+		ConvertToShort(samplesFloat, samplesShort);
 
-        ConvertToFloat (samplesShort, samplesFloat);
+		for (int i = 0; i < sizeOfChunk; i++) {
+			short sample = samplesShort[i];
+			inputChunk[i] = sample;
+		}
 
-        AudioClip a = AudioClip.Create ("test", samplesFloat.Length, 1, 1000, false);
-        a.SetData (samplesFloat, 0);
+		length = m_wide_enc.Encode(inputChunk, 0, inputChunk.Length, encoded, 0, encoded.Length);
 
-        GetComponent<AudioSource> ().clip = a;
-        GetComponent<AudioSource> ().Play ();
-        Debug.Log ("Recording sent.");
-    }
+		return encoded;
+	}
 
-    byte[] EncodeToMuLaw(AudioClip clip) {
-        float[] samplesFloat = new float[clip.samples * clip.channels];
-        short[] samplesShort = new short[clip.samples * clip.channels];
-        byte[] samplesByte = new byte[clip.samples * clip.channels];
-        clip.GetData (samplesFloat, 0);
+	AudioClip DecodeFromNSpeex(byte[] encoded, int length) {
+		float[] result = new float[encoded.Length];
+		short[] decoded = new short[encoded.Length];
+		NSpeex.SpeexDecoder m_wide_dec = new NSpeex.SpeexDecoder(NSpeex.BandMode.Wide);
+	
+		yo = m_wide_dec.Decode(encoded, 0, encoded.Length, decoded, 0, false);
 
-        ConvertToShort (samplesFloat, samplesShort);
+		ConvertToFloat(decoded, result);
 
-        for (int i = 0; i < samplesShort.Length; i++) {
-            samplesByte [i] = MuLawEncoder.LinearToMuLawSample (samplesShort[i]);
-        }
+		AudioClip a = AudioClip.Create("test", result.Length, 1, 12000, false);
+		a.SetData(result, 0);
 
-        return samplesByte;
-    }
-        
-//    short[] EncodeToNSpeex(AudioClip clip, out int length) {
-//        float[] samplesFloat = new float[clip.samples * clip.channels];
-//        short[] samplesShort = new short[clip.samples * clip.channels];
-//        int sizeOfChunk = 640 * (int) (Mathf.FloorToInt(samplesFloat.Length / 640f) - 1);
-//        short[] inputChunk = new short[sizeOfChunk];
-//        byte[] encoded = new byte[sizeOfChunk];
-//        NSpeex.SpeexEncoder m_wide_enc = new NSpeex.SpeexEncoder(NSpeex.BandMode.Wide);
-//
-//        clip.GetData(samplesFloat, 0);
-//        ConvertToShort(samplesFloat, samplesShort);
-//
-//        for (int i = 0; i < sizeOfChunk; i++) {
-//            short sample = samplesShort[i];
-//            inputChunk[i] = sample;
-//        }
-//
-//        length = m_wide_enc.Encode(inputChunk, 0, inputChunk.Length, encoded, 0, encoded.Length);
-//
-//        return inputChunk;
-//    }
-//
-//    AudioClip DecodeFromNSpeex(short[] encoded, int length) {
-//        float[] result = new float[encoded.Length];
-//        short[] decoded = new short[encoded.Length];
-//        NSpeex.SpeexDecoder m_wide_dec = new NSpeex.SpeexDecoder(NSpeex.BandMode.Wide);
-//    
-//        //yo = m_wide_dec.Decode(encoded, 0, encoded.Length, decoded, 0, false);
-//
-//        ConvertToFloat(encoded, result);
-//
-//        AudioClip a = AudioClip.Create("test", result.Length, 1, 12000, false);
-//        a.SetData(result, 0);
-//
-//        return a;
-//    }
+		return a;
+	}
 
-    void ConvertToShort(float[] samplesFloat, short[] samplesShort) {
-        for (int i = 0; i < samplesFloat.Length; i++) {
-            float sample = samplesFloat[i];
-            sample += 1f; // now it's in the range 0 .. 2
-            sample *= 0.5f; // now it's in the range 0 .. 1
+	void ConvertToShort(float[] samplesFloat, short[] samplesShort) {
+		for (int i = 0; i < samplesFloat.Length; i++) {
+			float sample = samplesFloat[i];
+			sample += 1f; // now it's in the range 0 .. 2
+			sample *= 0.5f; // now it's in the range 0 .. 1
 
-            short val = (short) Mathf.FloorToInt(sample * short.MaxValue);
+			short val = (short) Mathf.FloorToInt(sample * short.MaxValue);
 
-            samplesShort[i] = val;
-        }
-    }
+			samplesShort[i] = val;
+		}
+	}
 
-    void ConvertToFloat(short[] samples, float[] result) {
-        for (int i = 0; i < samples.Length; i++) {
-            float sample = samples[i];
-            sample /= (float) short.MaxValue;
-            sample *= 2f;
-            sample -= 1f;
+	void ConvertToFloat(short[] samples, float[] result) {
+		for (int i = 0; i < samples.Length; i++) {
+			float sample = samples[i];
+			sample /= (float) short.MaxValue;
+			sample *= 2f;
+			sample -= 1f;
 
-            result[i] = sample;
-        }
-    }
+			result[i] = sample;
+		}
+	}
 
-    void StartRecording() {
-        aud.clip = Microphone.Start(null, true, 1, 1000);
-    }
+	void StartRecording() {
+		aud.clip = Microphone.Start(null, true, 1, 12000);
+	}
 
-    void Update () {
-        if (!isLocalPlayer)
-            return;
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			Debug.Log ("Recording started.");
+			StartRecording (); 	
+		}
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
-        
-        if (Input.GetKeyDown (KeyCode.A)) {
-            Debug.Log ("Recording started.");
-            StartRecording ();     
-        }
-
-        if (Input.GetKeyDown (KeyCode.S)) {
-            Microphone.End (null);
-            Debug.Log ("Recording ended.");
-            byte[] encoded = EncodeToMuLaw(aud.clip);
-            CmdStopRecording (encoded);
-        }
-    }
+		if (Input.GetKeyDown (KeyCode.S)) {
+			Microphone.End (null);
+			Debug.Log ("Recording ended.");
+			CmdStopRecording ();
+		}
+	}
 }
