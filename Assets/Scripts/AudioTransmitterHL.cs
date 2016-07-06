31a7ac742fb4cfa3259ba973420ffe3aa223923c
Michael Laythe <mrlaythe24@aol.com>
2016-07-06 13:52:11 -0700

    implemented zlib for another layer of compression

--- Assets/Scripts/AudioTransmitterHL.cs
+++ Assets/Scripts/AudioTransmitterHL.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.Networking;
 using NAudio.Codecs;
+using Ionic.Zlib;
 
 public class AudioTransmitterHL : NetworkBehaviour {
 
@@ -18,6 +19,7 @@ public class AudioTransmitterHL : NetworkBehaviour {
 
 	[ClientRpc]
 	void RpcPlayAudio(byte[] encoded) {
+		encoded = ZlibDecompress (encoded, encoded.Length);
 		short[] samplesShort = new short[encoded.Length];
 		float[] samplesFloat = new float[encoded.Length];
 
@@ -27,7 +29,7 @@ public class AudioTransmitterHL : NetworkBehaviour {
 
 		ConvertToFloat (samplesShort, samplesFloat);
 
-		AudioClip a = AudioClip.Create ("test", samplesFloat.Length, 1, 1000, false);
+		AudioClip a = AudioClip.Create ("test", samplesFloat.Length, 1, 8000, false);
 		a.SetData (samplesFloat, 0);
 
 		GetComponent<AudioSource> ().clip = a;
@@ -110,7 +112,7 @@ public class AudioTransmitterHL : NetworkBehaviour {
 	}
 
 	void StartRecording() {
-		aud.clip = Microphone.Start(null, true, 1, 1000);
+		aud.clip = Microphone.Start(null, true, 1, 8000);
 	}
 
 	void Update () {
@@ -131,8 +133,35 @@ public class AudioTransmitterHL : NetworkBehaviour {
 		if (Input.GetKeyDown (KeyCode.S)) {
 			Microphone.End (null);
 			Debug.Log ("Recording ended.");
-			byte[] encoded = EncodeToMuLaw(aud.clip);
-			CmdStopRecording (encoded);
+			byte[] encodedWithMuLaw = EncodeToMuLaw(aud.clip);
+			byte[] encodedWithZLib = ZlibCompress (encodedWithMuLaw, encodedWithMuLaw.Length);
+			CmdStopRecording (encodedWithZLib);
+		}
+	}
+
+	byte[] ZlibCompress(byte[] input, int length)
+	{
+		using (var ms = new System.IO.MemoryStream())
+		{
+			using (var compressor = new Ionic.Zlib.ZlibStream(ms, CompressionMode.Compress, CompressionLevel.BestCompression))
+			{
+				compressor.Write(input, 0, length);
+			}
+
+			return ms.ToArray();
+		}
+	}
+
+	byte[] ZlibDecompress(byte[] input, int length)
+	{
+		using (var ms = new System.IO.MemoryStream())
+		{
+			using (var compressor = new Ionic.Zlib.ZlibStream(ms, CompressionMode.Decompress, CompressionLevel.BestCompression))
+			{
+				compressor.Write(input, 0, length);
+			}
+
+			return ms.ToArray();
 		}
 	}
 }
