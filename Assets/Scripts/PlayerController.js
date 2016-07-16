d599d1765d7d6f29874d798d233ed32bc668f40f
jeyip <jeremy.yip7@gmail.com>
2016-07-15 23:13:27 -0700

    fixed quick acceleration of player bug

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,9 +1,9 @@
 ﻿#pragma strict
 
 public var speed : float;
-public var AccelerometerUpdateInterval : float = 1.0 / 60.0;
-public var LowPassKernelWidthInSeconds : float = 1.0;
-public var LowPassFilterFactor : float = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;
+public var accelerometerUpdateInterval : float = 1.0 / 60.0;
+public var lowPassKernelWidthInSeconds : float = 1.0;
+public var lowPassFilterFactor : float = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
 private var movement : Vector3;
 private var rb : Rigidbody;
 private var lowPassValue : Vector3;
@@ -12,29 +12,28 @@ private var currentAccelerationMagnitude : float;
 private var accelerationDifference : float; 
 
 function Start() {
-  // accessing rigid body component of player game object
   rb = GetComponent.<Rigidbody>();
   lowPassValue = Input.acceleration;
   pastAccelerationMagnitude = 9.9;
-  movement = Vector3.zero;
 }
 
 function LowPassFilterAccelerometer() : Vector3 {
   // Smooths out noise from accelerometer data
-  lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);
+  lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, lowPassFilterFactor);
   return lowPassValue;
 }
 
 function FixedUpdate () {
+  movement = Vector3.zero;
   currentAccelerationMagnitude = LowPassFilterAccelerometer().magnitude;
   accelerationDifference = Mathf.Abs(pastAccelerationMagnitude - currentAccelerationMagnitude);
   
-  // movement of player in the direction of the camera
+  // Moves player in the direction of the camera
   if ( accelerationDifference > .0015 && .004 > accelerationDifference ) {
     movement = Camera.main.transform.forward;
   } 
 
-  pastAccelerationMagnitude = Mathf.Abs(LowPassFilterAccelerometer().magnitude);
+  pastAccelerationMagnitude = Mathf.Abs(currentAccelerationMagnitude);
 
   // Maps the player's real-world steps into in-game movement
   rb.AddForce(movement * speed);
