ad41786448519e0e3c3a4fc27e21dd6e64cd8331
jeyip <jeremy.yip7@gmail.com>
2016-07-15 23:03:48 -0700

    reorganizing code and initializing more variables at start of playercontroller script

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -4,15 +4,19 @@ public var speed : float;
 public var AccelerometerUpdateInterval : float = 1.0 / 60.0;
 public var LowPassKernelWidthInSeconds : float = 1.0;
 public var LowPassFilterFactor : float = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;
+private var movement : Vector3;
 private var rb : Rigidbody;
 private var lowPassValue : Vector3;
-private var temp : float;
+private var pastAccelerationMagnitude : float;
+private var currentAccelerationMagnitude : float;
+private var accelerationDifference : float; 
 
 function Start() {
   // accessing rigid body component of player game object
   rb = GetComponent.<Rigidbody>();
   lowPassValue = Input.acceleration;
-  temp = 9.9;
+  pastAccelerationMagnitude = 9.9;
+  movement = Vector3.zero;
 }
 
 function LowPassFilterAccelerometer() : Vector3 {
@@ -22,15 +26,15 @@ function LowPassFilterAccelerometer() : Vector3 {
 }
 
 function FixedUpdate () {
-  var movement : Vector3 = Vector3.zero;
-  var accelerationDifference : float = Mathf.Abs(temp - LowPassFilterAccelerometer().magnitude);
+  currentAccelerationMagnitude = LowPassFilterAccelerometer().magnitude;
+  accelerationDifference = Mathf.Abs(pastAccelerationMagnitude - currentAccelerationMagnitude);
   
   // movement of player in the direction of the camera
   if ( accelerationDifference > .0015 && .004 > accelerationDifference ) {
     movement = Camera.main.transform.forward;
   } 
 
-  temp = Mathf.Abs(LowPassFilterAccelerometer().magnitude);
+  pastAccelerationMagnitude = Mathf.Abs(LowPassFilterAccelerometer().magnitude);
 
   // Maps the player's real-world steps into in-game movement
   rb.AddForce(movement * speed);
