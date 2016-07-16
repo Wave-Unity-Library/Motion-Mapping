d224527309f96fd8aca86cea28438321483492a5
jeyip <jeremy.yip7@gmail.com>
2016-07-15 22:31:51 -0700

    Organizing public and private variables in PlayerController.js

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,23 +1,24 @@
 ﻿#pragma strict
 
-var speed : float;
-var AccelerometerUpdateInterval : float = 1.0 / 60.0;
-var LowPassKernelWidthInSeconds : float = 1.0;
-var LowPassFilterFactor : float = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;
+public var speed : float;
+public var AccelerometerUpdateInterval : float = 1.0 / 60.0;
+public var LowPassKernelWidthInSeconds : float = 1.0;
+public var LowPassFilterFactor : float = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;
 private var rb : Rigidbody;
 private var lowPassValue : Vector3;
-private var temp : float = 9.9;
+private var temp : float;
 
 function Start() {
   // accessing rigid body component of player game object
   rb = GetComponent.<Rigidbody>();
   lowPassValue = Input.acceleration;
+  temp = 9.9;
 }
 
 function LowPassFilterAccelerometer() : Vector3 {
-    // Smooths out noise from accelerometer data
-    lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);
-    return lowPassValue;
+  // Smooths out noise from accelerometer data
+  lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);
+  return lowPassValue;
 }
 
 function FixedUpdate () {
@@ -31,7 +32,6 @@ function FixedUpdate () {
 
   temp = Mathf.Abs(LowPassFilterAccelerometer().magnitude);
 
-  // Movement vector applied to player gameObject
   // Maps the player's real-world steps into in-game movement
   rb.AddForce(movement * speed);
 }
