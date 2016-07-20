133bdc005920cee9a4ffdb41899b5eb3d37742b4
jeyip <jeremy.yip7@gmail.com>
2016-07-20 10:45:21 -0700

    Updated all audio scripts

---
+++ Assets/Scripts/CircularBuffer.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CircularBuffer<T> {
+	T[] buffer;
+	int head;
+	int tail;
+	int bufferSize;
+	int length;
+
+	public CircularBuffer(int bufferSize) {
+		buffer = new T[bufferSize];
+		this.bufferSize = bufferSize;
+		head = this.bufferSize - 1;
+	}
+
+	public void Enqueue(T sample) {
+		head = NextPosition(head);
+		buffer[head] = sample;
+
+		if (IsFull)
+			tail = NextPosition(tail);
+		else
+			length++;
+	}
+
+	public T Dequeue() {
+		if (IsEmpty)
+			throw new UnityException("Queue is empty.");
+
+		T dequeued = buffer[tail];
+		tail = NextPosition(tail);
+		length--;
+
+		return dequeued;
+	}
+
+	int NextPosition(int pos) {
+		return (pos + 1) % bufferSize;
+	}
+
+	public T[] GetBuffer {
+		get { return buffer; }
+	}
+
+	public int GetHeadIndex {
+		get { return head; }
+	}
+
+	public int GetTailIndex {
+		get { return tail; }
+	}
+
+	public bool IsEmpty {
+		get { return length == 0; }
+	}
+
+	public bool IsFull {
+		get { return length == bufferSize; }
+	}
+
+	public int Length {
+		get { return length; }
+	}
+
+	public int BufferLength {
+		get { return bufferSize; }
+	}
+}
---
+++ Assets/Scripts/VoiceController.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using Ionic.Zlib;
+using System;
+using Voice;
+
+public class VoiceController : NetworkBehaviour {
+
+	AudioSource aud;
+	AudioClip clip;
+	CircularBuffer<float[]> cBuffer = new CircularBuffer<float[]>(5);
+	bool isTransmitting = false;
+	int recordFrequency;
+	int lastPos = 0;
+	int lastPlayed = 0;
+	float[] sampleBuffer;
+	float[] filtered;
+	float playbackDelay = 0;
+	bool isPlaying = false;
+	double percentPlayed = 0;
+
+	void Start () {
+		int minFreq;
+		int maxFreq;
+
+		Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
+
+		recordFrequency = minFreq == 0 && maxFreq == 0 ? 44100 : 16000;
+		Debug.Log (recordFrequency);
+
+		aud = GetComponent<AudioSource> ();
+		clip = AudioClip.Create ("test", recordFrequency, 1, recordFrequency, false);
+		aud.clip = clip;
+	}
+
+	[Command]
+	void CmdStopRecording (byte[] encoded) {
+		RpcPlayAudio(encoded);
+	}
+
+	[ClientRpc]
+	void RpcPlayAudio (byte[] encoded) {
+		if (lastPlayed >= recordFrequency)
+			lastPlayed -= recordFrequency;
+		
+		encoded = VoiceUtils.ZlibDecompress (encoded, encoded.Length);
+		float[] samplesFloat = new float[encoded.Length / 4];
+
+		Buffer.BlockCopy (encoded, 0, samplesFloat, 0, encoded.Length);	
+
+		percentPlayed = percentPlayed + ((double) samplesFloat.Length / aud.clip.samples);
+
+		GetComponent<AudioSource> ().clip.SetData (samplesFloat, lastPlayed);
+
+		if (!isPlaying) {
+			GetComponent<AudioSource> ().Play ();
+			aud.loop = true;
+			isPlaying = true;
+		}
+			
+		if (GetComponent<AudioSource> ().time >= percentPlayed) {
+//			GetComponent<AudioSource> ().Pause ();
+			isPlaying = false;
+			Debug.Log ("not caught up");
+		}
+
+		if (percentPlayed >= 1) {
+			percentPlayed = percentPlayed - 1;
+		}
+			
+		Debug.Log ("Recording sent.");
+		lastPlayed += samplesFloat.Length;
+	}
+
+	void ConvertToShort (float[] samplesFloat, short[] samplesShort) {
+		for (int i = 0; i < samplesFloat.Length; i++) {
+			float sample = samplesFloat[i];
+			sample += 1f; // now it's in the range 0 .. 2
+			sample *= 0.5f; // now it's in the range 0 .. 1
+
+			short val = (short) Mathf.FloorToInt (sample * short.MaxValue);
+
+			samplesShort[i] = val;
+		}
+	}
+
+	void ConvertToFloat (short[] samples, float[] result) {
+		for (int i = 0; i < samples.Length; i++) {
+			float sample = samples[i];
+			sample /= (float) short.MaxValue;
+			sample *= 2f;
+			sample -= 1f;
+
+			result[i] = sample;
+		}
+	}
+
+	void StartRecording () {
+		aud.clip = Microphone.Start (null, true, 1, recordFrequency);
+		while (Microphone.GetPosition (null) < 0) {}
+		isTransmitting = true;
+	}
+
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+
+		playbackDelay += Time.deltaTime;
+
+		if (isTransmitting) {			
+			int currentPos = Microphone.GetPosition (null);
+			int diff = currentPos - lastPos;
+			int partitionSize = recordFrequency / cBuffer.BufferLength;
+
+			if (currentPos < lastPos) {
+				diff = recordFrequency - lastPos + currentPos - 1;
+			}
+
+			if (diff >= partitionSize) {
+				sampleBuffer = new float[diff * aud.clip.channels];
+
+				aud.clip.GetData (sampleBuffer, lastPos);
+
+				VoiceUtils.Downsample (sampleBuffer, out filtered);
+
+				cBuffer.Enqueue (filtered);
+			
+				lastPos = currentPos;
+			}
+
+			if (!cBuffer.IsEmpty) {
+				if (playbackDelay >= 0.05f) {
+					byte[] sampleBytes = new byte[filtered.Length * 4];
+			
+					Buffer.BlockCopy (cBuffer.Dequeue (), 0, sampleBytes, 0, sampleBytes.Length);
+					byte[] encodedWithZLib = VoiceUtils.ZlibCompress (sampleBytes, sampleBytes.Length);
+					CmdStopRecording (encodedWithZLib);
+					playbackDelay = 0;
+				}
+			}
+				
+		}
+
+		if (Input.GetKeyDown (KeyCode.O)) {
+			Debug.Log ("Recording started.");
+			StartRecording (); 	
+		}
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			Microphone.End (null);
+			Debug.Log ("Recording ended.");
+			isTransmitting = false;
+		}
+	}
+
+	byte[] ZlibCompress (byte[] input, int length) {
+		using (var ms = new System.IO.MemoryStream ()) {
+			using (var compressor = new Ionic.Zlib.ZlibStream (ms, CompressionMode.Compress, CompressionLevel.BestCompression)) {
+				compressor.Write (input, 0, length);
+			}
+
+			return ms.ToArray ();
+		}
+	}
+
+	byte[] ZlibDecompress (byte[] input, int length) {
+		using (var ms = new System.IO.MemoryStream ()) {
+			using (var compressor = new Ionic.Zlib.ZlibStream (ms, CompressionMode.Decompress, CompressionLevel.BestCompression)) {
+				compressor.Write (input, 0, length);
+			}
+
+			return ms.ToArray ();
+		}
+	}
+}
