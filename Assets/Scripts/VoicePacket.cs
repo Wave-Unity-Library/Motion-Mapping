689ba64d100d0b6097b9af6cea167b5464cfbd18
Michael Laythe <mrlaythe24@aol.com>
2016-07-22 09:23:06 -0700

    every piece of audio data send is now encapsulated by a voicepacket class

---
+++ Assets/Scripts/VoicePacket.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Voice {
+	public struct VoicePacket {
+		public byte[] data;
+		public int length;
+		public VoiceCompression compression;
+	}
+}
